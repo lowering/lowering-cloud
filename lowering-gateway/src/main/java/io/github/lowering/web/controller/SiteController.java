88072619896451f95212deb6ad1eac307eef68dc
Lowering <iYiwen@126.com>
2017-12-07 19:29:32 +0800

    添加站点控制器

---
+++ lowering-gateway/src/main/java/io/github/lowering/web/controller/SiteController.java
@@ -0,0 +1,11 @@
+package io.github.lowering.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/site")
+public class SiteController {
+
+
+}
