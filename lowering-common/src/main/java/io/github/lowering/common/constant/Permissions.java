800706173d4d3754e20b34ffdb08df41d824b486
Lowering <iYiwen@126.com>
2017-12-07 19:28:54 +0800

    添加权限常量

---
+++ lowering-common/src/main/java/io/github/lowering/common/constant/Permissions.java
@@ -0,0 +1,10 @@
+package io.github.lowering.common.constant;
+
+public interface Permissions {
+    String USER_VIEW = "USER_VIEW";
+    String USER_SAVE = "USER_SAVE";
+    String USER_UPDATE = "USER_UPDATE";
+    String USER_DELETE = "USER_DELETE";
+    String ROLE_VIEW = "ROLE_VIEW";
+    String ROLE_SAVE = "ROLE_SAVE";
+}
