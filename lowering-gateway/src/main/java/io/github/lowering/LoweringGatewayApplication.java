933cfadac4b0aed1a1be5d4e85052bcf1ea34d20
lowering <iYiwen@126.com>
2017-12-10 15:55:56 +0800

    修改配置添加首页

--- lowering-gateway/src/main/java/io/github/lowering/LoweringGatewayApplication.java
+++ lowering-gateway/src/main/java/io/github/lowering/LoweringGatewayApplication.java
@@ -2,19 +2,70 @@ package io.github.lowering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@EnableZuulProxy
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableZuulProxy
 public class LoweringGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoweringGatewayApplication.class, args);
+	}
+
+	@Configuration
+	protected static class WebMvcConfiguration extends WebMvcConfigurerAdapter {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			super.addCorsMappings(registry);
+			registry.addMapping("/*");
+		}
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	@EnableGlobalMethodSecurity(prePostEnabled = true)
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+	protected static class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests()
+					.antMatchers("/","/account/**").permitAll()
+					.anyRequest().authenticated()
+					.and()
+					.csrf().disable();
+		}
+
+		@Override
+		public void configure(WebSecurity web) throws Exception {
+			web.ignoring().antMatchers("/webjars/**");
+		}
+	}
+
+	@Configuration
+	@EnableResourceServer
+	@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER - 2)
+	protected static class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+		@Override
+		public void configure(HttpSecurity http) throws Exception {
+			http
+					.authorizeRequests()
+					.antMatchers("/","/account/**","/webjars/**").permitAll()
+					.anyRequest().authenticated();
+		}
 	}
 }
--- lowering-gateway/src/main/java/io/github/lowering/web/controller/IndexController.java
+++ lowering-gateway/src/main/java/io/github/lowering/web/controller/IndexController.java
@@ -1,6 +1,7 @@
 package io.github.lowering.web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +13,8 @@ import org.springframework.web.servlet.ModelAndView;
 public class IndexController {
 
     @GetMapping
-    public String index(){
-        System.out.println("index");
+    public String index(Model model){
+        model.addAttribute("title","后台管理系统");
         return "index";
     }
 }
