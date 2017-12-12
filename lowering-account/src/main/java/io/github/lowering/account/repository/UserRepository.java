8c7bc5e30232cad3f6f23ce2a28a79abb39b279d
Lowering <iYiwen@126.com>
2017-12-12 14:09:55 +0800

    添加Repository实现和更新已有的实现

--- lowering-account/src/main/java/io/github/lowering/account/repository/UserRepository.java
+++ lowering-account/src/main/java/io/github/lowering/account/repository/UserRepository.java
@@ -1,9 +1,11 @@
 package io.github.lowering.account.repository;
 
 import io.github.lowering.account.domain.User;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends CrudRepository<User,String>{
+public interface UserRepository extends CrudRepository<User,String> {
+
 }
