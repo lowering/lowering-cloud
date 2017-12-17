7ec61feace0e75b9a58ce4a6628718fc79870d70
lowering <iYiwen@126.com>
2017-12-17 19:06:25 +0800

    新增异常处理器

---
+++ lowering-common/src/main/java/io/github/lowering/common/web/advice/DefaultHandlerExceptionAdvice.java
@@ -0,0 +1,35 @@
+package io.github.lowering.common.web.advice;
+
+import io.github.lowering.common.result.ErrorResult;
+import io.github.lowering.common.result.Result;
+import io.github.lowering.common.result.ValidationResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.validation.ConstraintViolationException;
+import java.util.Objects;
+
+@ControllerAdvice
+public class DefaultHandlerExceptionAdvice {
+
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(Exception.class)
+    public Result global(Exception exception){
+        return new ErrorResult(100500, exception.getMessage());
+    }
+
+    @ResponseBody
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ConstraintViolationException.class)
+    public Result validation(ConstraintViolationException exception){
+        ValidationResult result = new ValidationResult(100400, exception.getMessage());
+        exception.getConstraintViolations().stream().filter(error-> Objects.nonNull(error.getPropertyPath())).forEach(error->result.setError(error.getPropertyPath().toString(),error.getMessage()));
+        return result;
+    }
+
+}
