a698e7b9ed94b88060316494f87a55fa56d999f0
lowering <iYiwen@126.com>
2017-12-17 19:05:26 +0800

    添加验证规则

--- lowering-account/src/main/java/io/github/lowering/account/domain/User.java
+++ lowering-account/src/main/java/io/github/lowering/account/domain/User.java
@@ -2,8 +2,13 @@ package io.github.lowering.account.domain;
 
 import com.fasterxml.jackson.annotation.JsonView;
 import io.github.lowering.account.domain.enums.Sex;
+import org.hibernate.validator.constraints.Email;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotBlank;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -18,19 +23,33 @@ public class User extends Id {
     @Column(length = 300)
     @JsonView(WithoutPasswordJView.class)
     private String description;
+
+    @Email(message = "{account.user.email}")
     @Column(length = 100)
     @JsonView(WithoutPasswordJView.class)
     private String email;
+
+    @NotNull(message = "{account.user.enabled.not-null}")
     @JsonView(WithoutPasswordJView.class)
     private Boolean enabled;
+
+    @NotNull(message = "{account.user.locked.not-null}")
     @JsonView(WithoutPasswordJView.class)
     private Boolean locked;
+
+    @NotNull(message = "{account.user.password.not-null}")
+    @Length(message = "{account.user.password.length}")
     @Column(length = 100, nullable = false)
     @JsonView(WithPasswordJView.class)
     private String password;
+
+    @NotNull(message = "{account.user.sex.not-null}")
     @Enumerated
     @JsonView(WithoutPasswordJView.class)
     private Sex sex;
+
+    @NotBlank(message = "{account.user.username.not-blank}")
+    @Length(message = "{account.user.username.length}")
     @Column(length = 100, unique = true, nullable = false)
     @JsonView(WithoutPasswordJView.class)
     private String username;
