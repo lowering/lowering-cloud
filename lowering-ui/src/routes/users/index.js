57be9e22d55cddcd5faaa890a178481fad1fd518
lowering <iYiwen@126.com>
2017-12-24 18:22:52 +0800

    用户操作

--- lowering-ui/src/routes/users/index.js
+++ lowering-ui/src/routes/users/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import mixin from '../../utils/mixin';
 import { connect } from 'dva';
-import {withRouter} from 'dva/router';
-import { Card, Table } from 'antd';
-import PageLayout from '../../layouts/page.layout';
-import UsersModel from '../../models/users';
+import OverviewLayout from '../../layouts/overview.layout';
+import { Card, Table, Badge } from 'antd';
 
 class Users extends React.PureComponent {
-
 
     componentDidMount(){
         const {dispatch} = this.props;
         dispatch({type:"users/findAll"});
     }
-
 
     render() {
 
@@ -29,17 +24,32 @@ class Users extends React.PureComponent {
             title: '邮箱',
             dataIndex: 'email',
             key: 'email',
+        }, {
+            title: '状态',
+            render (row) {
+                if (row.enabled) {
+                    return (<Badge status="success" text="启用" />);
+                }
+                return (<Badge status="error" text="禁用" />);
+            }
+        }, {
+            title: '备注',
+            dataIndex: 'description',
+            key: 'description',
         }];
 
         let { users } = this.props;
 
         return (
-            <PageLayout title="用户管理" content="用于提供对用户的各种操作。">
+            <OverviewLayout title="用户管理" content="用于提供对用户的各种操作。">
                 <Card bordered={false}>
                     <Table dataSource={users.users} columns={columns} rowKey='id'/>
                 </Card>
-            </PageLayout>
+            </OverviewLayout>
         )
     }
 }
-export default withRouter(connect(({users})=>({users}))(mixin([UsersModel],Users)));
+export default connect(state=>{
+    console.log(state);
+    return {users:state.users}
+})(Users);
