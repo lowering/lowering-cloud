f53fa17d9b474281dda57e89fb5cba7d7b3e9713
lowering <iYiwen@126.com>
2017-12-24 18:23:07 +0800

    路由表

---
+++ lowering-ui/src/routes/index.js
@@ -0,0 +1,98 @@
+import { uuid } from '../utils';
+/**
+ * 路由表
+ *
+ * 包含菜单和路由信息
+ *
+ */
+export const routes = [{
+    id: uuid(),
+    name: '工作台',
+    path: 'dashboard',
+    constant: '',
+    icon: 'dashboard',
+    target: '_self',
+    shown: true,
+    description: "提供个人的工作台",
+    models: [],
+    component: ()=>import('./dashboard')
+}, {
+    id: uuid(),
+    name: "系统设置",
+    constant: '',
+    path: "setting",
+    icon: "setting",
+    target: "_self",
+    shown: true,
+    description: "提供系统相关设置",
+    children: [{
+        id: uuid(),
+        name: "用户管理",
+        constant: '',
+        path: "users",
+        target: "_self",
+        shown: true,
+        description: "提供用户管理相关操作",
+        models: [import('../models/users')],
+        component: ()=>import('./users')
+    }, {
+        id: uuid(),
+        name: "角色管理",
+        constant: '',
+        path: "roles",
+        target: "_self",
+        shown: true,
+        description: "提供角色管理相关操作",
+        models: [import('../models/roles')],
+        component: ()=>import('./roles')
+    }, {
+        id: uuid(),
+        name: "资源管理",
+        constant: '',
+        path: "menus",
+        target: "_self",
+        shown: true,
+        description: "提供菜单和权限管理相关操作",
+        models:[import('../models/menus')],
+        component: ()=>import('./menus')
+    }]
+}, {
+    id: uuid(),
+    name: "组织机构管理",
+    constant: '',
+    path: "company",
+    target: "_self",
+    icon: 'contacts',
+    shown: true,
+    description: "提供组织机构管理相关操作",
+    children: [{
+        id: uuid(),
+        name: "组织管理",
+        path: "organization",
+        target: "_self",
+        shown: true,
+        description: "提供组织管理相关操作",
+        models: [import('../models/organization')],
+        component: ()=>import('./company/organization')
+    }, {
+        id: uuid(),
+        name: "部门管理",
+        constant: '',
+        path: "department",
+        target: "_self",
+        shown: true,
+        description: "提供部门管理相关操作",
+        models: [import('../models/department')],
+        component: ()=>import('./company/department')
+    }, {
+        id: uuid(),
+        name: "员工管理",
+        constant: '',
+        path: "employee",
+        target: "_self",
+        shown: true,
+        description: "提供员工管理相关操作",
+        models: [import('../models/employee')],
+        component: ()=>import('./company/employee')
+    }]
+}];
