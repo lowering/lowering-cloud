699545a5ff29b4972ea6c60df5f102b9ad0f032d
lowering <iYiwen@126.com>
2017-12-24 18:18:36 +0800

    实现页脚

--- lowering-ui/src/components/footer/index.js
+++ lowering-ui/src/components/footer/index.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Layout } from 'antd';
 
 import styles from './index.less';
 
-export default () => {
+export default ({className,links,copyright}) => {
+
     return (
-       <div className={styles.footer}>
-           Footer
-       </div> 
+        <Layout.Footer style={{padding:0}}>
+            <div className={[styles.footer,className].join(' ')}>
+                {
+                    links && (
+                        <div className={styles.links}>
+                            {links.map(link => (
+                                <a
+                                    key={link.title}
+                                    target={link.target || '_self'}
+                                    href={link.href}
+                                >
+                                    {link.title}
+                                </a>
+                            ))}
+                        </div>
+                    )
+                }
+                {copyright && <div className={styles.copyright}>{copyright}</div>}
+            </div>
+        </Layout.Footer>
     );
 }
