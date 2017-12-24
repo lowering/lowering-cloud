2012d37df00f42c9eec17b75c2d85796dcf16872
lowering <iYiwen@126.com>
2017-12-24 18:21:41 +0800

    更新Dashboard

--- lowering-ui/src/routes/dashboard/index.js
+++ lowering-ui/src/routes/dashboard/index.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import mixin from '../../utils/mixin';
-import { connect } from 'dva';
+import OverviewLayout from '../../layouts/overview.layout';
 
-import PageLayout from '../../layouts/page.layout';
-
-
-class Dashboard extends React.Component{
-
-    render () {
-        return (
-            <PageLayout>
-                <div>dashboard</div>
-            </PageLayout>
-        );
-    }
+export default () => {
+    return (
+        <OverviewLayout title="Dashboard">
+            <div>Dashboard</div>
+        </OverviewLayout>
+    );
 }
-export default mixin([],Dashboard);
