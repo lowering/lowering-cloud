e7f98e554d6339eca00bd3b763dcdc8de59eda9f
Lowering <iYiwen@126.com>
2017-12-28 17:42:54 +0800

    添加抽屉效果

---
+++ lowering-ui/src/components/drawer/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './index.less';
+
+class Drawer extends React.PureComponent {
+
+    render () {
+        return (
+            <div>Drawer</div>
+        );
+    }
+}
+
+export default Drawer;
