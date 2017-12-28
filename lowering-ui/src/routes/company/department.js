8dcc6fa8de1e854b5156617cd7efd4832ea1541e
Lowering <iYiwen@126.com>
2017-12-28 17:43:41 +0800

    在overview中显示组织信息

--- lowering-ui/src/routes/company/department.js
+++ lowering-ui/src/routes/company/department.js
@@ -1,7 +1,51 @@
 import React from 'react';
 import { connect } from 'dva';
-import { Card, Table } from 'antd';
+import { Card, Table, Button, Menu, Dropdown, Icon } from 'antd';
 import OverviewLayout from "../../layouts/overview.layout";
+import Descriptions from '../../components/descriptions';
+
+
+const menu = (
+    <Menu>
+        <Menu.Item key="1">选项一</Menu.Item>
+        <Menu.Item key="2">选项二</Menu.Item>
+        <Menu.Item key="3">选项三</Menu.Item>
+    </Menu>
+);
+
+const action = (
+    <div>
+        <Button type="primary" icon="plus">添加菜单</Button>
+        <Button.Group>
+            <Button>删除菜单</Button>
+            <Button>操作</Button>
+            <Dropdown overlay={menu} placement="bottomRight">
+                <Button><Icon type="ellipsis" /></Button>
+            </Dropdown>
+        </Button.Group>
+    </div>
+);
+
+const columns = [{
+    title: '部门名称',
+    dataIndex: 'name',
+    key: 'name',
+}, {
+    title: '唯一建',
+    dataIndex: 'constant',
+    key: 'constant',
+}, {
+    title: '备注',
+    dataIndex: 'description',
+    key: 'description',
+}];
+
+const descriptions = (
+    <Descriptions size="small" col="2">
+        <Descriptions.Description term="所属组织">北京总部</Descriptions.Description>
+        <Descriptions.Description term="备注">北京总部</Descriptions.Description>
+    </Descriptions>
+);
 
 class Department extends React.PureComponent {
 
@@ -12,24 +56,10 @@ class Department extends React.PureComponent {
 
     render () {
 
-        const columns = [{
-            title: '部门名称',
-            dataIndex: 'name',
-            key: 'name',
-        }, {
-            title: '唯一建',
-            dataIndex: 'constant',
-            key: 'constant',
-        }, {
-            title: '备注',
-            dataIndex: 'description',
-            key: 'description',
-        }];
-
         const { department } = this.props;
 
         return (
-            <OverviewLayout title="部门管理" content="提供部门相关的操作">
+            <OverviewLayout title="部门管理" content={descriptions} action={action}>
                 <Card bordered={false}>
                     <Table dataSource={department.departments} columns={columns} rowKey="id"/>
                 </Card>
